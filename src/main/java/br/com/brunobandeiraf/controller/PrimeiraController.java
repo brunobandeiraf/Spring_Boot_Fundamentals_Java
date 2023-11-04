7cb350aa71cec81a13c62aa433fb13f9a3d68e0c
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-11-04 11:38:14 -0300

    body params

--- src/main/java/br/com/brunobandeiraf/controller/PrimeiraController.java
+++ src/main/java/br/com/brunobandeiraf/controller/PrimeiraController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,4 +33,14 @@ public class PrimeiraController {
     public String metodoComQueryParamsMultiplos(@RequestParam Map<String, String> allParams){
         return "O parâmetro com  metodoComQueryParamsMultiplos é" + allParams.entrySet();
     }
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario){
+        return "metodoComBodyParams " + usuario.username();
+    }
+
+    // Criando uma classe simples, utilizando o record 
+    // sem obrigação de criar construtores e métodos getters e setters
+    record Usuario(String username){}
+
 }
